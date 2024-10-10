765824296df00802c41e1ba7b4f304759e2d154a
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2024-10-10 10:57:42 +0100

    fix frontend

--- server.js
+++ server.js
@@ -37,8 +37,14 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 })
 
+/**
+ * -: makes public folder avalible to add assets
+ * -: we can now take a pic, place it in public folder and use path name to view that img
+ * -: eventually the whole frontend wil sit
+ * -: we set up the public to utilize the uploads
+ */
 const __dirname = dirname(fileURLToPath(import.meta.url))
-app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 
 app.use(cookieParser())
 app.use(express.json())
@@ -55,11 +61,12 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter)
 app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
 
+// send entry point
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 })
 
-// catc all
+// catch all for not found
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' })
 })
