e474140a737cc169cec348ed97848308312dda48
adamEllisMacBook <125833852+adamEllisMacBook@users.noreply.github.com>
2024-10-11 11:29:24 +0100

    fix update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,9 @@ import User from '../models/userModel.js'
 import { StatusCodes } from 'http-status-codes'
 
 import cloudinary from 'cloudinary'
+import { formatImage } from '../middleware/multerMiddleware.js'
 
-import { promises as fs } from 'fs'
+// import { promises as fs } from 'fs'
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId })
@@ -18,15 +19,21 @@ export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ users, jobs })
 }
 
+/**
+ * saving in the memory 
+ * formating the buffer into something cloudinary can understand
+ */
 export const updateUser = async (req, res) => {
   console.log(req.file)
   const newUser = { ...req.body }
   delete newUser.password
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path)
+    const file = formatImage(req.file)
+    // return // cuts the code off for testing only
+    // await fs.unlink(req.file.path) // used for disk storage
+    const response = await cloudinary.v2.uploader.upload(file)
     console.log(response)
-    await fs.unlink(req.file.path)
 
     newUser.avatar = response.secure_url
     newUser.avatarPublicId = response.public_id
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,16 +1,33 @@
 import multer from 'multer'
+import DataParser from 'datauri/parser.js'
+import path from 'path'
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads')
-  },
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/uploads')
+//   },
 
-  filename: (req, file, cb) => {
-    const fileName = file.originalname
+//   filename: (req, file, cb) => {
+//     const fileName = file.originalname
 
-    cb(null, fileName)
-  },
-})
+//     cb(null, fileName)
+//   },
+// })
+// const upload = multer({ storage })
+
+const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
+const parser = new DataParser()
+
+// export helper file
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString()
+  // console.log(fileExtension)
+  return parser.format(fileExtension, file.buffer).content
+  // console.log(file)
+  // console.log(file.buffer)
+}
+
+// one default per file
 export default upload
